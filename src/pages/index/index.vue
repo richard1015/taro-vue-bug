7a1091d5b0e60e72343d61dd086fbdce7de94310
zhuzhida <zhuzhida@jd.com>
2020-09-17 21:14:28 +0800

    init

---
+++ src/pages/index/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <view class="index">
+    <text class="nut-test">{{ msg }}</text>
+  </view>
+</template>
+
+<script>
+import './index.scss'
+
+export default {
+  data () {
+    return {
+      msg: 'Hello world!'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.nut-test{
+  margin-top: 30px;
+}
+</style>
